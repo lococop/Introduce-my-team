89f322e627ece2345b473f8f150fe6ab27b02401
gwan woo Ryu <58384645+lococop@users.noreply.github.com>
2020-05-07 22:25:11 +0900

    6th homework
    webview my github page

---
+++ MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.mygithubpageapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        WebView myWebView = (WebView) findViewById(R.id.webview);
+        myWebView.loadUrl("https://lococop.github.io/");
+    }
+}
